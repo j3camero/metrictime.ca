635ab9a6543e1beed7d91728e09b7df9e83d8154
Ubuntu <ubuntu@ip-172-31-29-211.ca-central-1.compute.internal>
2022-01-15 21:51:10 +0000

    Minimal working site with a crappy UI.

---
+++ index.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    setInterval(Update, 200);
+});
+
+function Update() {
+    const now = new Date();
+    const gregorianYear = now.getFullYear();
+    const holoceneYear = gregorianYear + 10000;
+    const start = new Date(gregorianYear, 0, 0);
+    const diff = now - start;
+    const oneDay = 1000 * 60 * 60 * 24;
+    const dayOfYear = Math.floor(diff / oneDay);
+    const dateAndYearText = `Day ${dayOfYear} of year ${holoceneYear}`;
+    const midnight = new Date();
+    midnight.setHours(0, 0, 0, 0);
+    const msSinceMidnight = now - midnight;
+    const secondsSinceMidnight = Math.floor(msSinceMidnight / 1000);
+    const ks = secondsSinceMidnight / 1000;
+    const timeOfDayLabel = document.getElementById('timeofday');
+    timeOfDayLabel.innerText = ks.toFixed(3);
+    const dateAndYearLabel = document.getElementById('dateandyear');
+    dateAndYearLabel.innerText = dateAndYearText;
+}
